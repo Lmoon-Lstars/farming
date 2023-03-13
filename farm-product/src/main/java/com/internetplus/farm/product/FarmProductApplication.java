14caa8f68c8d861c942dcadb357dc423f216d34b
JackCc <1181153997@qq.com>
2023-03-13 21:02:23 +0800

    各个模块nacos注册，降低springboot版本，对应springcloud版本

--- farm-product/src/main/java/com/internetplus/farm/product/FarmProductApplication.java
+++ farm-product/src/main/java/com/internetplus/farm/product/FarmProductApplication.java
@@ -3,8 +3,10 @@ package com.internetplus.farm.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.internetplus.farm.product.dao")
 public class FarmProductApplication {
 
--- farm-supplier/src/main/java/com/internetplus/farm/supplier/FarmSupplierApplication.java
+++ farm-supplier/src/main/java/com/internetplus/farm/supplier/FarmSupplierApplication.java
@@ -5,11 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ServletComponentScan
 @EnableAsync
+@EnableDiscoveryClient
 @MapperScan("com.internetplus.farm.supplier.dao")
 public class FarmSupplierApplication {
 
